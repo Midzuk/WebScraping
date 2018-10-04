11de25562ad44095e238a0067d2501beb07fc69c
Midzuki <ucdmzk@icloud.com>
2018-10-04 12:16:02 +0900

    修正

--- mesh-statistics.py
+++ mesh-statistics.py
@@ -2,14 +2,34 @@ import urllib.request
 import zipfile
 import os
 import time
+import csv
+import glob
 
-url = 'https://www.e-stat.go.jp/gis/statmap-search/data?statsId=T000876&code=3622&downloadType=2'
+meshes = []
 
-urllib.request.urlretrieve(url, 'output/sample.zip')
+with open('mesh-code.csv', 'r') as f:
+    reader = csv.reader(f)
 
-with zipfile.ZipFile('output/sample.zip') as zip:
-    zip.extractall('output/')
+    for row in reader:
+        meshes.append(row)
 
-os.remove('output/sample.zip')
+for mesh in meshes:
+    url = 'https://www.e-stat.go.jp/gis/statmap-search/data?statsId=T000876&code=%s&downloadType=2' % mesh
 
-time.sleep(1)
+    urllib.request.urlretrieve(url, 'output/temporary/_.zip')
+
+    with zipfile.ZipFile('output/temporary/_.zip') as zip:
+        zip.extractall('output/temporary')
+
+    os.remove('output/temporary/_.zip')
+
+    time.sleep(0.5)
+
+temps = glob.glob('output/temporary*')
+
+print(temps)
+
+'''
+with open('open.csv', 'a') as output:
+    pass
+'''
